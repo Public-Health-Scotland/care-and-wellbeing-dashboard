6eaa14978894e85d1d22503ad8b03c93bf47de09
Jennifer Evans <jennie04@nssrstudio05.csa.scot.nhs.uk>
2023-01-16 12:10:02 +0000

    updated 'fair work..' indicator

--- shiny_app/functions/modals.R
+++ shiny_app/functions/modals.R
@@ -205,7 +205,7 @@ observeEvent(input$unmanageable_debt_comment,
              ))
 
 
-observeEvent(input$employees_living_wage_modal_info,
+observeEvent(input$employees_living_wage_2_modal_info,
              showModal(
                modalDialog(
                  title = "Background information and source",
@@ -234,6 +234,14 @@ observeEvent(input$employees_living_wage_modal_info,
              ))
 
 observeEvent(input$employees_living_wage_modal_comment,
+             showModal(
+               modalDialog(
+                 title = "Summary of indicator during covid and pre-covid period",
+                 #p("The proportion of employees earning less than the Living Wage has decreased from 18.8% in 2012 to 14.4% in 2021; and decreased 0.7 percentage points from 2020 to 2021. The proportion of employees earning less than the Living Wage is now lower than at any previous point in the series, which began in 2012. This indicates that performance is maintaining."),
+                 easyClose = TRUE, fade=FALSE, footer = modalButton("Close (Esc)"))
+             ))
+
+observeEvent(input$employees_living_wage_2_modal_comment,
              showModal(
                modalDialog(
                  title = "Summary of indicator during covid and pre-covid period",
@@ -303,7 +311,38 @@ observeEvent(input$gender_pay_gap_modal_info,
                  easyClose = TRUE, fade=FALSE, footer = modalButton("Close (Esc)"))
              ))
 
+observeEvent(input$pay_gap_modal_info,
+             showModal(
+               modalDialog(
+                 title = "Background information and source",
+                 h4("Indicator"),
+                 p("Difference between male and female full-time hourly earnings, as % of male full time hourly earnings."),
+                 h4("Source"),
+                 p("Annual Survey of Hours and Earnings, ONS"),
+                 h4("Notes"),
+                 tags$div(
+                   tags$ul(
+                     tags$li(
+                       "Estimates for employees aged 16+ on the PAYE system on adult rates whose pay for the survey pay-period was not affected by absence. Estimates for 2020 and ",
+                       "have been furloughed under the Coronavirus Job Retention Scheme (CJRS)."
+                     ),
+                     tags$li(
+                       "2021 data are provisional."
+                     )
+                   )
+                 ),
+                 easyClose = TRUE, fade=FALSE, footer = modalButton("Close (Esc)"))
+             ))
+
 observeEvent(input$gender_pay_gap_modal_comment,
+             showModal(
+               modalDialog(
+                 title = "Summary of indicator during covid and pre-covid period",
+                 #p("The gender pay gap has decreased from 8.4% in 2012. In 2021, the gender pay gap for Scotland was 3.0%, an increase of 0.6 percentage points on the previous year. This indicates that performance is maintaining."),
+                 easyClose = TRUE, fade=FALSE, footer = modalButton("Close (Esc)"))
+             ))
+
+observeEvent(input$pay_gap_modal_comment,
              showModal(
                modalDialog(
                  title = "Summary of indicator during covid and pre-covid period",
@@ -551,6 +590,24 @@ observeEvent(input$skills_shortage_vacancies_modal_info,
 
 
 observeEvent(input$economic_inactivity_modal_info,
+             showModal(
+               modalDialog(
+                 title = "Background information and source",
+                 h4("Economic Inactivity"),
+
+                 p("This indicator measures the proportion percentage of economically inactive adults 16-64 who want to work ",
+                   "using the Annual Population Survey (Scotland's Labour Market - People, Places and Regions)."),
+
+                 p("The proportion of those who were economically inactive and would like to work increased by 0.9 percentage points, ",
+                   "from 20.7 per cent in April 2019-March 2020 (the lowest point in the series to date) to 21.6 per cent in April 2020-March 2021."),
+
+                 tags$a(href ="https://www.gov.scot/collections/labour-market-statistics/",
+                        "Source: Annual Population Survey 2020"),
+                 easyClose = TRUE, fade=FALSE, footer = modalButton("Close (Esc)"))
+
+             ))
+
+observeEvent(input$economic_inactivity_2_modal_info,
              showModal(
                modalDialog(
                  title = "Background information and source",
---
+++ shiny_app/indicators/care_and_wellbeing/fair_work/functions_fair_work.R
@@ -0,0 +1,126 @@
+##############################################.
+# EMPLOYEES ON THE LIVING WAGE----
+##############################################.
+
+employees_living_wage_2_plot_line = function(data, title = NULL, color_column = NULL) {
+
+
+  if (is.null(color_column))
+  {
+    color_column_option = NULL
+  } else{
+    color_column_option = data[[color_column]]
+  }
+
+
+
+  data %>%
+    plot_ly(x=~year,
+            y=~measure_value,
+            type="scatter",
+            mode="lines+markers",
+            color = color_column_option,
+            colors = palette,
+            text = paste0("Sector: ", color_column_option, "<br>",
+                          "Year: ", data$year, "<br>",
+                          "Proportion (%): ", data$measure_value,"% <br>"),
+            hoverinfo = "text") %>%
+    layout(xaxis = list(title = "Year"),
+           yaxis = list(title = "Proportion (%)", range = c(0,100), ticksuffix = "%"),
+           title = title,
+           legend = list(bgcolor = 'rgba(0,0,0,0)', title = list(text = "<b>Double click on one legend <br>to isolate one trace</b>"))) %>%
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove)
+}
+
+
+##############################################.
+# GENDER PAY GAP----
+##############################################.
+
+
+pay_gap_plot_line = function(data, title = NULL, second_axis = FALSE) {
+
+  fig = data %>%
+    plot_ly(x=~year,
+            y=~`Pay Gap`,
+            type="scatter",
+            mode="lines+markers",
+            name = "Pay Gap \n(Woman/Men) (%)",
+            #color = color_column_option,
+            #colors = palette,
+            text = paste0("Year: ", data$year, "<br>",
+                          "Pay Gap (Woman/Men) (%): ", data$`Pay Gap`,"% <br>"),
+            hoverinfo = "text")
+
+  if(second_axis)
+  {
+    fig = fig %>% add_trace(x=~year,
+                            y=~Men,
+                            yaxis = "y2",
+                            type="scatter",
+                            mode="lines+markers",
+                            name = "Men (£)",
+                            line = list(dash = 'dash'),
+                            text = paste0("Year: ", data$year, "<br>",
+                                          "Men Median Hourly Earnings (£): ", data$Men," <br>"),
+                            hoverinfo = "text")
+    fig = fig %>% add_trace(x=~year,
+                            y=~Woman,
+                            yaxis = "y2",
+                            type="scatter",
+                            mode="lines+markers",
+                            name = "Women (£)",
+                            line = list(dash = 'dash'),
+                            text = paste0("Year: ", data$year, "<br>",
+                                          "Women Median Hourly Earnings (£): ", data$Men," <br>"),
+                            hoverinfo = "text")  %>%
+      layout(yaxis2 =  list(title = "Median Hourly Earnings (£)",
+                            overlaying = "y",
+                            side = "right",
+                            rangemode="tozero",
+                            #gridcolor = "blue",
+                            showgrid = FALSE))
+
+
+  }
+
+  fig = fig %>%
+    layout(xaxis = list(title = "Year"),
+           yaxis = list(title = "Pay Gap (Woman/Men) (%)", range = c(-100,100), ticksuffix = "%"),
+           title = title,
+           showlegend = TRUE,
+           legend = list(x = 1.2, y = 1, bgcolor = 'rgba(0,0,0,0)', title = list(text = "<b>Double click on one legend <br>to isolate one trace</b>"))) %>%
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove)
+
+  fig
+
+}
+
+
+plot_economic_inactivity_2 <- function(data) {
+
+  fig = data %>%
+    plot_ly(x = ~year,
+            y = ~percent,
+            color = ~breakdown,
+            type="scatter",
+            mode="lines+markers",
+            colors = palette,
+            text = paste0("Year: ", data$year, "<br>",
+                          "Proportion (%): ",
+                          data$percent, "%",
+                          "<br>",
+                          "Number of Vacancies: ",
+                          format(data$n, big.mark = ",")),
+            hoverinfo = "text",
+            textposition="none") %>%
+    layout(yaxis = yaxis_proportion,
+           xaxis = xaxis_year) %>%
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove)
+
+
+}
+
+
+
+
