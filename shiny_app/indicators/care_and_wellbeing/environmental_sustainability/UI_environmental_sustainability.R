ba027176dbe7f2f8000d2cfaa20dc040d8b4c7aa
Jennifer Evans <jennie04@nssrstudio04.csa.scot.nhs.uk>
2023-01-17 08:53:47 +0000

    updated 'pursuing environmental...' indicator

---
+++ shiny_app/indicators/care_and_wellbeing/environmental_sustainability/UI_environmental_sustainability.R
@@ -0,0 +1,39 @@
+tagList(
+  h1("Pursuing environmental sustainability and health equity together" ),
+  br(),
+  h4(strong("Select indicator: ")),
+  navlistPanel(widths = c(2,10),
+
+               ##############################################.
+               # ACCESS TO GREEN AND BLUE SPACE----
+               ##############################################.
+               tabPanel(title = "Access to green and blue space",
+                        value = "blue_green_space",
+                        icon = icon_no_warning_fn("lemon"),
+
+                        h3("Access to green and blue space")
+               ),
+
+               ##############################################.
+               # VISITS TO THE OUTDOORS----
+               ##############################################.
+
+               tabPanel(title = "Visits to the outdoors",
+                        value = "visit_outdoors",
+                        icon = icon_no_warning_fn("lemon"),
+
+                        h3("Visits to the outdoors")
+               ),
+
+               ##############################################.
+               # JOURNEYS BY ACTIVE TRAVEL----
+               ##############################################.
+
+               tabPanel(title = "Journeys by active travel",
+                        value = "journeys_active",
+                        icon = icon_no_warning_fn("lemon"),
+
+                        h3("Journeys by active travel")
+               )
+  )
+)
