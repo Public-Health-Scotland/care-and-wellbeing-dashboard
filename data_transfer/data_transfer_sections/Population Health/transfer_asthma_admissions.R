d2900a8c93b161540c4469da93f0bef2c9648a30
Jennifer Evans <jennie04@nssrstudio05.csa.scot.nhs.uk>
2023-01-25 11:35:12 +0000

    data transfer for asthma

--- data_transfer/data_transfer_sections/Population Health/transfer_asthma_admissions.R
+++ data_transfer/data_transfer_sections/Population Health/transfer_asthma_admissions.R
@@ -7,42 +7,50 @@ data_sheet_1920 <- "Data3"
 data_sheet_2021 <- "Data4"
 data_sheet_2122 <- "Data5"
 
-input_asthma_admissions_1718 <- read_excel(data_path, sheet = data_sheet_1718, col_names = TRUE) %>%
-  filter(grepl('Asthma', lookup))
-input_asthma_admissions_1819 <- read_excel(data_path, sheet = data_sheet_1819, col_names = TRUE) %>%
-  filter(grepl('Asthma', lookup))
-input_asthma_admissions_1920 <- read_excel(data_path, sheet = data_sheet_1920, col_names = TRUE) %>%
-  filter(grepl('Asthma', lookup))
-input_asthma_admissions_2021 <- read_excel(data_path, sheet = data_sheet_2021, col_names = TRUE) %>%
-  filter(grepl('Asthma', lookup))
-input_asthma_admissions_2122 <- read_excel(data_path, sheet = data_sheet_2122, col_names = TRUE) %>%
-  filter(grepl('Asthma', lookup))
+
+input_asthma_admissions_1718 <- read_excel(data_path, sheet = data_sheet_1718, col_names = TRUE)
+input_asthma_admissions_1819 <- read_excel(data_path, sheet = data_sheet_1819, col_names = TRUE)
+input_asthma_admissions_1920 <- read_excel(data_path, sheet = data_sheet_1920, col_names = TRUE)
+input_asthma_admissions_2021 <- read_excel(data_path, sheet = data_sheet_2021, col_names = TRUE)
+input_asthma_admissions_2122 <- read_excel(data_path, sheet = data_sheet_2122, col_names = TRUE)
 
 
 input_asthma_admissions <- rbind(input_asthma_admissions_1718, input_asthma_admissions_1819, input_asthma_admissions_1920,
-                                 input_asthma_admissions_2021, input_asthma_admissions_2122)
+                                 input_asthma_admissions_2021, input_asthma_admissions_2122) %>%
+  filter(grepl('Asthma', lookup))
+
 
 input_asthma_admissions %<>%
   select(lookup, stays_Number, stays_Rate) %>%
-  separate(lookup, into = c("Date","Everything"), sep = 7) %>%
-  separate(Everything, into = c("Everything", "Ages"), sep = " - ")
+  mutate(lookup = str_remove(lookup, pattern = "Asthma")) %>%
+  separate(lookup, into = c("Date","lookup"), sep = 7) %>%
+  separate(lookup, into = c("lookup", "Ages"), sep = " - ")
 
 input_asthma_admissions_male <- input_asthma_admissions %>%
-  filter(grepl("Male", Everything)) %>%
-  separate(Everything, into = c("Everything", "Sex"), sep = -4)
+  filter(grepl("Male", lookup)) %>%
+  separate(lookup, into = c("lookup", "Sex"), sep = -4)
 
 input_asthma_admissions_female <- input_asthma_admissions %>%
-  filter(grepl("Female", Everything)) %>%
-  separate(Everything, into = c("Everything", "Sex"), sep = -6)
+  filter(grepl("Female", lookup)) %>%
+  separate(lookup, into = c("lookup", "Sex"), sep = -6)
 
 input_asthma_admissions_all <- input_asthma_admissions %>%
-  filter(grepl("All Sexes", Everything)) %>%
-  separate(Everything, into = c("Everything", "Sex"), sep = -9)
+  filter(grepl("All Sexes", lookup)) %>%
+  separate(lookup, into = c("lookup", "Sex"), sep = -9)
 
 input_asthma_admissions <- rbind(input_asthma_admissions_all, input_asthma_admissions_female, input_asthma_admissions_male)
 
+input_asthma_admissions %<>%
+  mutate(Provisional = ifelse(str_starts(lookup, "p"),"1","0"),
+         lookup = ifelse(Provisional == "1", substring(lookup,2), lookup)) %>%
+  mutate(geography_type = ifelse(lookup == "Scottish Residents", "Scotland",
+                                 ifelse(str_starts(lookup, "NHS"), "Health Board", "Other"))
 
-input <- input_asthma_admissions %>%
-  str_remove(input_asthma_admissions$Ages, pattern = "Asthma")
 
 
+replace_file_fn(input_asthma_admissions,
+                paste0(path_out, "/asthma_admissions.rds"))
+
+rm(input_asthma_admissions, input_asthma_admissions_1718, input_asthma_admissions_1819,
+   input_asthma_admissions_1920, input_asthma_admissions_2021, input_asthma_admissions_2122,
+   input_asthma_admissions_all, input_asthma_admissions_female, input_asthma_admissions_male)
