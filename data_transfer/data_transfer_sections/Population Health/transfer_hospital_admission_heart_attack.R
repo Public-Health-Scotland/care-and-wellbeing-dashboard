cc8c94631ce02d7bbfec86d113d05a04100ce899
jennie04 <jennifer.evans4@phs.scot>
2023-03-08 15:12:46 +0000

    read in excel file

---
+++ data_transfer/data_transfer_sections/Population Health/transfer_hospital_admission_heart_attack.R
@@ -0,0 +1,5 @@
+##############################################.
+# HOSPITAL ADMISSIONS HEART ATTACK ----
+##############################################.
+
+heart_attack <- read_excel(paste0(path_in_pop, "/heart-attack-hospital-admissions-table-4.1.xlsx"))
