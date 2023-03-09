e42659205c2fa1c0a391386350da6ba49a779821
jennie04 <jennifer.evans4@phs.scot>
2023-03-09 07:50:58 +0000

    create the hopsital_admission_heart_attack transfer file

--- data_transfer/data_transfer_sections/Population Health/transfer_hospital_admission_heart_attack.R
+++ data_transfer/data_transfer_sections/Population Health/transfer_hospital_admission_heart_attack.R
@@ -2,4 +2,16 @@
 # HOSPITAL ADMISSIONS HEART ATTACK ----
 ##############################################.
 
-heart_attack <- read_excel(paste0(path_in_pop, "/heart-attack-hospital-admissions-table-4.1.xlsx"))
+heart_attack <- read_excel(paste0(path_in_pop, "/heart-attack-hospital-admissions-table-4.1.xlsx")) %>%
+  rename("date" = Year) %>%
+  clean_names() %>%
+  select(-target_population_size) %>%
+  summary_format_function(date = .$date,
+                          geog_type = "Scotland",
+                          geog = "Scotland",
+                          indicator_in = .$total_admissions,
+                          value_in = "hospital_admission_heart_attack")
+
+replace_file_fn(heart_attack, paste0(path_out, "/heart_attack.rds"))
+
+rm(heart_attack)
