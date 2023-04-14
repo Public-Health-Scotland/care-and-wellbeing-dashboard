9014334f61f418e8c31d4aed8d754fd395ae2e26
jennie04 <jennifer.evans4@phs.scot>
2023-04-14 16:37:42 +0100

    update infoBoxes with spinners and change place holder text

--- shiny_app/indicators/summary/UI_summary_v2.R
+++ shiny_app/indicators/summary/UI_summary_v2.R
@@ -33,108 +33,108 @@ tagList(
                                    fluidRow(
                                      column(4,
                                             h4("Admissions for asthma"),
-                                            infoBoxOutput("asthma_admissions_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("asthma_admissions_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Alcohol-related deaths"),
-                                            infoBoxOutput("alcohol_deaths_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("alcohol_deaths_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Alcohol-related hospital admissions"),
-                                            infoBoxOutput("alcohol_admissions_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("alcohol_admissions_infobox", width=NULL)),
                                      )
                                    ),
 
                                    fluidRow(
                                      column(4,
                                             h4("All-cause mortality (age 15-44)"),
-                                            infoBoxOutput("all_cause_mortality_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("all_cause_mortality_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Coronary heart disease (CHD): deaths (age 45-74)"),
-                                            infoBoxOutput("chd_deaths_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("chd_deaths_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Drug-related deaths"),
-                                            infoBoxOutput("drug_deaths_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("drug_deaths_infobox", width=NULL)),
                                      )),
 
                                    fluidRow(
                                      column(4,
                                             h4("Drug-related hospital admissions"),
-                                            infoBoxOutput("drug_admissions_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("drug_admissions_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Experience of social care recipients"),
-                                            infoBoxOutput("experience_recipients_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("experience_recipients_infobox", width=NULL)),
                                      ),
                                      column(4,
                                             h4("Experience of unpaid carers"),
-                                            infoBoxOutput("experience_of_unpaid_carers_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("experience_of_unpaid_carers_infobox", width=NULL)),
                                      )),
 
                                    fluidRow(
                                      column(3,
                                             h4("First ever hospital admission for heart attack (under 75)"),
-                                            infoBoxOutput("hospital_admission_heart_attack_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("hospital_admission_heart_attack_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Health risk behaviours"),
-                                            infoBoxOutput("health_risk_behaviours_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("health_risk_behaviours_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Healthy birthweight"),
-                                            infoBoxOutput("healthy_birthweight_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("healthy_birthweight_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Healthy life expectancy"),
-                                            infoBoxOutput("healthy_life_expectancy_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("healthy_life_expectancy_infobox", width=NULL)),
                                      )),
 
                                    fluidRow(
                                      column(3,
                                             h4("Healthy weight adults"),
-                                            infoBoxOutput("healthy_weight_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("healthy_weight_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Limiting long-term conditions (age 16+)"),
-                                            infoBoxOutput("adult_long_term_condition_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("adult_long_term_condition_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Mental wellbeing of adults (16+)"),
-                                            infoBoxOutput("mental_wellbeing_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("mental_wellbeing_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Physical activity"),
-                                            infoBoxOutput("physical_activity_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("physical_activity_infobox", width=NULL)),
                                      )),
 
                                    fluidRow(
                                      column(3,
                                             h4("Premature mortality"),
-                                            infoBoxOutput("premature_mortality_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("premature_mortality_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Quality of care experience"),
-                                            infoBoxOutput("quality_care_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("quality_care_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Screening- uptake for breast and bowel cancer"),
-                                            infoBoxOutput("screening_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("screening_infobox", width=NULL)),
                                      ),
                                      column(3,
                                             h4("Self-assessed health of adults (age 16+)"),
-                                            infoBoxOutput("adults_self_assessed_health_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("adults_self_assessed_health_infobox", width=NULL)),
                                      )),
 
                                    fluidRow(
                                      column(6,
                                             h4("Vaccinations – uptake "),
-                                            infoBoxOutput("vaccinations_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("vaccinations_infobox", width=NULL)),
                                      ),
                                      column(6,
                                             h4("Work-related ill health"),
-                                            infoBoxOutput("work_related_health_infobox", width=NULL),
+                                            withSpinner(infoBoxOutput("work_related_health_infobox", width=NULL)),
                                      ))
                                  )),
 
@@ -146,37 +146,37 @@ tagList(
                                    fluidRow(
                                      column(6,
                                             h4("At risk of obesity"),
-                                            infoBoxOutput("children_at_risk_of_obesity_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("children_at_risk_of_obesity_infobox", width=NULL))
                                      ),
                                      column(6,
                                             h4("Child material deprivation"),
-                                            infoBoxOutput("child_material_deprivation_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("child_material_deprivation_infobox", width=NULL))
                                      )),
 
                                    fluidRow(
                                      column(6,
                                             h4("Child social and physical development"),
-                                            infoBoxOutput("child_development_cw_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("child_development_cw_infobox", width=NULL))
                                      ),
                                      column(6,
                                             h4("Child wellbeing and happiness"),
-                                            infoBoxOutput("child_wellbeing_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("child_wellbeing_infobox", width=NULL))
                                      )),
 
                                    fluidRow(
                                      column(6,
                                             h4("Infant mortality"),
-                                            infoBoxOutput("infant_mortality_cw_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("infant_mortality_cw_infobox", width=NULL))
                                      ),
                                      column(6,
                                             h4("Perinatal mortality rate"),
-                                            infoBoxOutput("perinatal_mortality_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("perinatal_mortality_infobox", width=NULL))
                                      )),
 
                                    fluidRow(
                                      column(12,
                                             h4("Physical activity"),
-                                            infoBoxOutput("physical_activity_children_cw_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("physical_activity_children_cw_infobox", width=NULL))
                                      ))
                                  )),
 
@@ -188,27 +188,27 @@ tagList(
                                    fluidRow(
                                      column(6,
                                             h4("CAHMS waiting times"),
-                                            infoBoxOutput("camhs_waiting_times_cw_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("camhs_waiting_times_cw_infobox", width=NULL))
                                      ),
                                      column(6,
                                             h4("Children have positive relationships"),
-                                            infoBoxOutput("children_relationships_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("children_relationships_infobox", width=NULL))
                                      )),
 
                                    fluidRow(
                                      column(6,
                                             h4("Children’s voices"),
-                                            infoBoxOutput("childrens_voices_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("childrens_voices_infobox", width=NULL))
                                      ),
                                      column(6,
                                             h4("Confidence and resilience of children and young people"),
-                                            infoBoxOutput("confidence_of_young_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("confidence_of_young_infobox", width=NULL))
                                      )),
 
                                    fluidRow(
                                      column(12,
                                             h4("Mental health / mental wellbeing"),
-                                            infoBoxOutput("mental_health_cw_infobox", width=NULL)
+                                            withSpinner(infoBoxOutput("mental_health_cw_infobox", width=NULL))
                                      ))
                                  ))
                     ),
