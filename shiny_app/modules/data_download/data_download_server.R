591715bd25444622b6e6e7ebfd69706f65fec632
markha06 <mark.hamilton4@phs.scot>
2023-04-21 11:16:04 +0100

    Updating module to have two inputs for filtered and unfiltered data

--- shiny_app/modules/data_download/data_download_server.R
+++ shiny_app/modules/data_download/data_download_server.R
@@ -1,7 +1,7 @@
 ####################### Modals #######################
 
 # Data download  server ----
-dataDownloadServer <- function(data, id, filename,
+dataDownloadServer <- function(data, data_download = data, id, filename,
                                add_separator_cols = NULL, # with , separator and 0dp
                                add_separator_cols_1dp = NULL, # with , separator and 1dp
                                add_separator_cols_2dp = NULL, # with , separator and 2dp,
@@ -82,11 +82,11 @@ dataDownloadServer <- function(data, id, filename,
 
       date_of_download <- format(today(), "%Y%m%d")
 
-      table_colnames = names(data) %>%
+      table_colnames = names(data_download) %>%
         gsub("_", " ", .) %>%
         str_to_sentence(.)
 
-      data_output <- data %>%
+      data_output <- data_download %>%
         rename_at(vars(names(data)), ~ table_colnames)
 
       output$csv <- downloadHandler(
