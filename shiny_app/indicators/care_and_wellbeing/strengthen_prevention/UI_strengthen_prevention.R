9aa218666c95bed1af9564a9a8409d2ec5d2428e
jennie04 <jennifer.evans4@phs.scot>
2023-05-09 16:10:48 +0100

    add information into drugs section

--- shiny_app/indicators/care_and_wellbeing/strengthen_prevention/UI_strengthen_prevention.R
+++ shiny_app/indicators/care_and_wellbeing/strengthen_prevention/UI_strengthen_prevention.R
@@ -233,7 +233,12 @@ tagList(
                                tabPanel(title = "Drug-related hospital admissions",
                                         value = "drug_admissions",
 
-                                        h2("Drug-related hospital admissions", iButtonUI("drug_admissions", content = "Paste background info and source for drug admissions here")),
+                                        h2("Drug-related hospital admissions", iButtonUI("drug_admissions",
+                                                                                         content = paste("This indicator uses data from Public Health Scotland drug related hospital",
+                                                                                         "statistics publication relating to general acute and psychiatric hospital stays with",
+                                                                                         "a diagnosis of drug misuse. These data are presented at a national level and broken",
+                                                                                         "down by demographic characteristics/local geographies. More information can be found",
+                                                                                         "<a href = https://www.publichealthscotland.scot/publications/show-all-releases?id=32315 target=_blank> here (external link). </a>"))),
 
 
                                         selectizeInput("drug_admissions_age", "Select an age group",
@@ -243,7 +248,7 @@ tagList(
                                                        options = list(plugins = c('remove_button'),
                                                                       `actions-box` = TRUE)),
 
-                                        altTextUI("drug_admission_alt"),
+                                        altTextUI("drug_admissions_alt"),
                                         withSpinner(plotlyOutput("drug_admissions_plot")),
 
                                         br(),
@@ -261,7 +266,11 @@ tagList(
                                tabPanel(title = "Drug-related deaths",
                                         value = "drug_deaths",
 
-                                        h2("Drug-related deaths", iButtonUI("drug_deaths", content = "Paste background info and source for drug deaths here")),
+                                        h2("Drug-related deaths", iButtonUI("drug_deaths",
+                                                                            content = paste("Statistics of drug-related deaths in 2021 and earlier years,",
+                                                                            "broken down by age, sex, substances implicated in the death, underlying",
+                                                                            "cause of death, and NHS Board and Council areas can be found",
+                                                                            "<a href = https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/deaths/drug-related-deaths-in-scotland target = _blank> here (external link). </a>"))),
 
                                         fluidRow(
                                           column(3,
