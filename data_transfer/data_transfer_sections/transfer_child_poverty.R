f2e49eb20ec69205f4ef5844753e696b455d80ca
iainma09 <iain.mackinnon2@phs.scot>
2023-05-11 08:30:08 +0100

    change data source to use 3 yearly average data sets from 2023 SG publication

--- data_transfer/data_transfer_sections/transfer_child_poverty.R
+++ data_transfer/data_transfer_sections/transfer_child_poverty.R
@@ -4,11 +4,9 @@
 # Read in the data
 
 
-poverty <- read_excel(paste0(path_in, "/data2023_1yr.xlsx"),
-                      sheet = "1", skip = 4, n_max = 4)%>%
-  mutate("2020/21" = "",
-         "2020/21"=as.numeric("2020/21"))%>%
-  pivot_longer(cols = 2:ncol(.),
+poverty <- read_excel(paste0(path_in, "/data2023.xlsx"),
+                      sheet = "1", skip = 6, n_max = 4)%>%
+   pivot_longer(cols = 2:ncol(.),
                names_to = "financial_year",
                values_to = "proportion") %>%
   filter(Group %in% c("Children", "All people")) %>%
