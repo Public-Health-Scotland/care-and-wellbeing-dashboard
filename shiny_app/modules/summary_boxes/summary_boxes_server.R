f44d4556b9c14c362a41f0a3c3fd4411961257ca
jennie04 <jennifer.evans4@phs.scot>
2023-05-16 16:03:11 +0100

    updates to summary box server

--- shiny_app/modules/summary_boxes/summary_boxes_server.R
+++ shiny_app/modules/summary_boxes/summary_boxes_server.R
@@ -1,10 +1,12 @@
 
 
-summaryBoxServer <- function(id, recent_date,# = recent_date,
-                             previous_date,# = previous_date,
-                             recent_value,# = recent_value,
-                             previous_value,# = previous_value,
-                             change) {# = change) {
+summaryBoxServer <- function(id, recent_date,
+                             previous_date,
+                             recent_value,
+                             previous_value,
+                             change,
+                             percentage_symbol = "" # If a percentage symbol is needed update to "%"
+) {
 
   moduleServer(
     id,
@@ -14,16 +16,19 @@ summaryBoxServer <- function(id, recent_date,# = recent_date,
       output$summary_values <- renderUI(
 
         tagList(
+
           div(class = "recent-value",
-              glue("{recent_date}: ",
-                   "{ifelse(length(recent_value)[1] == 0,'Not available', recent_value)}")),
+              ifelse(length(recent_value)[1] == 0,'Not available', glue("{recent_date}: {format(round_half_up(recent_value,2),big.mark=',')}{percentage_symbol}"))),
 
           div(class = "previous-value",
-              glue("{previous_date}: ",
-                   "{ifelse(length(previous_value)[1] == 0,'Not available', previous_value)}")),
+              ifelse(length(previous_value)[1] == 0,'', glue("{previous_date}: {format(round_half_up(previous_value,2),big.mark=',')}{percentage_symbol}"))),
+
 
           div(class = "value-change",
-              glue("Percentage change: +{round_half_up(change,2)}%")),
+              glue("Percentage change: ",
+                   "{ifelse(change < 0,'','+')}{round_half_up(change,2)}%")),
+          br()
+
 
         )
 
