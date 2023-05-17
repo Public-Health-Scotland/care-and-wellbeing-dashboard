1abafb541bcec1a303755576a5b15af785794fe8
jennie04 <jennifer.evans4@phs.scot>
2023-05-17 14:37:33 +0100

    add br() after the box for the summary box module

--- shiny_app/modules/summary_boxes/summary_boxes_ui.R
+++ shiny_app/modules/summary_boxes/summary_boxes_ui.R
@@ -4,14 +4,17 @@ summaryBoxUI <- function(id, title = "", button_title = "", button_content = "To
 
   ns <- NS(id)
 
-  box(id = ns(id),
-      status = "primary",
-      title = div(class = "summary-box-title", glue("{title}")),
-                 summaryButtonUI(ns("summary_info"),
-                                 button_title,
-                                 button_content),
-      width = "100%",
-      uiOutput(ns("summary_values"))
+  tagList(
+    box(id = ns(id),
+        status = "primary",
+        title = div(class = "summary-box-title", glue("{title}")),
+        summaryButtonUI(ns("summary_info"),
+                        button_title,
+                        button_content),
+        width = "100%",
+        withSpinner(uiOutput(ns("summary_values")))
+    ),
+    br()
   )
 
 
