ab2198a31d247f74af42c0cf33db0f57b502652d
markha06 <mark.hamilton4@phs.scot>
2023-05-18 12:19:37 +0100

    Fix dropdown width issue

--- shiny_app/indicators/care_and_wellbeing/control_lives/UI_control_lives.R
+++ shiny_app/indicators/care_and_wellbeing/control_lives/UI_control_lives.R
@@ -33,7 +33,9 @@ tagList(
                                          selectInput("camhs_waiting_times_cw_geog_type",
                                          "Step 1: Select national or local geography level",
                                          choices = c("Scotland", "Health Board"),
-                                         selected = "Scotland")),
+                                         selected = "Scotland",
+                                         width = "100%")),
+
                                  column(4,
                                         selectInput("camhs_waiting_times_cw_geog_name",
                                           "Step 2: Select national or local geography area",
