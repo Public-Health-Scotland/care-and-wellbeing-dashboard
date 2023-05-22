468c2dca3fdae0288c73ded76020524dbbf0cb6c
calumm07 <calum.massie2@phs.scot>
2023-05-22 15:34:41 +0100

    hle functions

---
+++ shiny_app/indicators/care_and_wellbeing/strengthen_prevention/functions_strengthen_prevention.R
@@ -0,0 +1,89 @@
+confidence_line_function_hle = function(data, y_title, x_title = "Year range", title = "") {
+
+  yaxis_number[["title"]] = y_title
+  xaxis_year[["title"]] = x_title
+
+  plot_ly(data = data) %>%
+
+    add_trace(x=~date, y=~lower_confidence_interval,
+              type = "scatter",
+              mode = "lines",
+              line = list(color = 'transparent'),
+              name = "Lower confidence interval",
+              showlegend = TRUE,
+              hovertemplate = ~glue("{lower_confidence_interval %>% round_half_up(2)}"),
+              hoverinfo = "none",
+              textposition = "none",
+              legendgroup = "z_confidence_interval") %>%
+    add_trace(x=~date,
+              y=~upper_confidence_interval,
+              type = "scatter",
+              mode = "lines",
+              fill = 'tonexty',
+              fillcolor = phsstyles::phs_colours("phs-liberty-30"),
+              line = list(color = 'transparent'),
+              name = "Upper confidence interval",
+              showlegend = TRUE,
+              hovertemplate = ~glue("{upper_confidence_interval %>% round_half_up(2)}"),
+              hoverinfo = "none",
+              textposition = "none",
+              legendgroup = "z_confidence_interval") %>%
+    add_trace(x=~date,
+              y=~indicator,
+              type = "scatter",
+              mode = "lines",
+              name = "Healthy life expectancy",
+              line = list(color = phsstyles::phs_colours("phs-purple")),
+              showlegend = TRUE,
+              hovertemplate = ~glue("{indicator %>% round_half_up(2)}"),
+              hoverinfo = "text",
+              textposition = "none",
+              legendgroup = "rate") %>%
+    layout(xaxis = xaxis_year, yaxis = yaxis_number,
+           legend = list(xanchor = "center", x = 0.5, y = -0.3, orientation = 'h'),
+           title = list(text = str_wrap(title, width = 60), font = title_style),
+           margin = list(t = 90, b = 40),
+           hovermode = "x unified") %>%
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove)
+
+}
+
+confidence_scatter_function_hle = function(data, y_title, x_title = "Year range", title = "") {
+
+  yaxis_number[["title"]] = y_title
+  xaxis_year[["title"]] = x_title
+
+  filtered_data <- data %>%
+    filter(geography_type == "Council Area")
+
+  plot_ly(data = filtered_data) %>%
+
+    add_lines(x = ~geography, y = ~indicator,
+              line = list(color = "white"),
+              name = 'Healthy life expectancy',
+              error_y = ~list(array = ErrorBarHeight/2,
+                              arrayminus = ErrorBarLowerHeight,
+                              symmetric = FALSE,
+                              width = 0.5,
+                              color = phsstyles::phs_colours("phs-purple")),
+              marker = list(color = phsstyles::phs_colours("phs-purple"),
+                            size = 5)) %>%
+    add_segments(data=data, inherit = FALSE, text = NULL, hoverinfo = "text",
+                 x = min(data$geography), xend = max(data$geography), y = (data %>% filter(geography == "Scotland",
+                                                                                           date == "2019-21") %>%
+                                                                             .$indicator),
+                 yend=(data %>% filter(geography == "Scotland",
+                                       date == "2019-21") %>%
+                         .$indicator),
+                 type = "line",
+                 name = "Scotland",
+                 line = list(color = "#C73918",width=1,dash = "dash")) %>%
+
+  layout(xaxis = xaxis_council_area, yaxis = yaxis_number,
+         legend = list(xanchor = "center", x = 0.5, y = -0.3, orientation = 'h'),
+         title = list(text = str_wrap(title, width = 60), font = title_style),
+         margin = list(t = 90, b = 40),
+         hovermode = "x unified") %>%
+    config(displaylogo = F, displayModeBar = TRUE, modeBarButtonsToRemove = bttn_remove)
+
+}
