10eb4bcb67306d2e2ac46662e52bcdd7b6e02438
jennie04 <jennifer.evans4@phs.scot>
2023-05-24 15:37:36 +0100

    add transfer for breast screening

---
+++ data_transfer/data_transfer_sections/Population Health/transfer_screening.R
@@ -0,0 +1,42 @@
+###################### SCREENING UPTAKE ############################
+
+
+##### BREAST #############
+
+breast_board_path <- paste0(path_in_pop, "breast_screening_uptake_by_board.xlsx")
+breast_simd_path <- paste0(path_in_pop, "breast_screening_uptake_by_SIMD.xlsx")
+
+
+input_breast_board <- read_excel(breast_board_path, range = "A7:M24") %>%
+  rename("geography" = `NHS Board of Residence`) %>%
+  filter(!is.na(geography))
+
+
+previous_years <- ncol(input_breast_board)-2
+current_year <- ncol(input_breast_board)-0
+
+breast_by_board <- input_breast_board %>%
+  select(1, previous_years:current_year) %>%
+  mutate(geography_type = ifelse(geography == "Scotland", "Scotland", "Health Board"),
+         geography = str_replace(geography, "&", "and"),
+         geography = ifelse(geography == "Scotland", geography, paste("NHS", geography))) %>%
+  pivot_longer(cols = starts_with("2"), names_to = "year_range", values_to = "percentage_uptake")
+
+screening_breast_board <- breast_by_board
+
+input_breast_simd <- read_excel(breast_simd_path, range = "A6:S12") %>%
+  select(-starts_with(".")) %>%
+  rename("SIMD" = "SIMD2 Deprivation Category") %>%
+  filter(!is.na(SIMD)) %>%
+  pivot_longer(cols = 2:16, names_to = "geography", values_to = "percentage_uptake") %>%
+  mutate(geography_type = ifelse(geography == "Scotland", "Scotland", "Health Board"),
+         geography = ifelse(geography == "Scotland", geography, paste("NHS", geography))) %>%
+  arrange(geography, SIMD)
+
+
+screening_breast_simd <- input_breast_simd
+
+
+##########################
+# input_breast_simd <- read_excel(breast_simd_path, sheet = "KPI_2", range = "A16:Q39")
+
