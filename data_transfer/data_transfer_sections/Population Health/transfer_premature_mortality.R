ca9852840dfec4e0929f672c08787e45d3b298fc
markha06 <mark.hamilton4@phs.scot>
2023-05-25 09:54:49 +0100

    Further updates to data transfer

--- data_transfer/data_transfer_sections/Population Health/transfer_premature_mortality.R
+++ data_transfer/data_transfer_sections/Population Health/transfer_premature_mortality.R
@@ -12,49 +12,51 @@ hb_rate_full <- read_excel(data_path, sheet = "data for tab 3",
 hb_rate <- hb_rate_full %>%
   select(1:17) %>%
   row_to_names(row_number = 1)
-names(hb_rate)[1] <- "Year"
+names(hb_rate)[1] <- "date"
 names(hb_rate)[2] <- "cause"
 hb_rate <- hb_rate %>%
   filter(cause == "All causes of death") %>%
   pivot_longer(cols = Scotland:`Western Isles`,
-               names_to = "geography_name",
-               values_to = "Rate") %>%
-  mutate(Rate = round_half_up(as.numeric(Rate), 1))
+               names_to = "geography",
+               values_to = "indicator") %>%
+  mutate(indicator = round_half_up(as.numeric(indicator), 1))
 
 # Rates - LCL
 hb_rate_lcl <- hb_rate_full %>%
   select(1, 18:33) %>%
   row_to_names(row_number = 1)
-names(hb_rate_lcl)[1] <- "Year"
+names(hb_rate_lcl)[1] <- "date"
 names(hb_rate_lcl)[2] <- "cause"
 hb_rate_lcl <- hb_rate_lcl %>%
   filter(cause == "All causes of death") %>%
   pivot_longer(cols = Scotland:`Western Isles`,
-               names_to = "geography_name",
-               values_to = "Rate_LCL") %>%
-  mutate(Rate_LCL = round_half_up(as.numeric(Rate_LCL), 1))
+               names_to = "geography",
+               values_to = "lower_confidence_interval") %>%
+  mutate(lower_confidence_interval = round_half_up(as.numeric(lower_confidence_interval), 1))
 
 # Rates - UCL
 hb_rate_ucl <- hb_rate_full %>%
   select(1, 34:49) %>%
   row_to_names(row_number = 1)
-names(hb_rate_ucl)[1] <- "Year"
+names(hb_rate_ucl)[1] <- "date"
 names(hb_rate_ucl)[2] <- "cause"
 hb_rate_ucl <- hb_rate_ucl %>%
   filter(cause == "All causes of death") %>%
   pivot_longer(cols = Scotland:`Western Isles`,
-               names_to = "geography_name",
-               values_to = "Rate_UCL") %>%
-  mutate(Rate_UCL = round_half_up(as.numeric(Rate_UCL), 1))
+               names_to = "geography",
+               values_to = "upper_confidence_interval") %>%
+  mutate(upper_confidence_interval = round_half_up(as.numeric(upper_confidence_interval), 1))
 
 # Join together
 hb_rate <- hb_rate %>%
   left_join(hb_rate_lcl) %>%
   left_join(hb_rate_ucl) %>%
   select(-cause) %>%
-  filter(Year >= 2008) %>%
-  mutate(geography_type = ifelse(geography_name == "Scotland",
-                                 "Scotland", "Health Board"))
+  filter(date >= 2008) %>%
+  mutate(geography_type = ifelse(geography == "Scotland",
+                                 "Scotland", "Health Board")) %>%
+  mutate(geography = ifelse(geography_type == "Health Board",
+                            paste0("NHS ", geography), geography))
 
 rm(hb_rate_full, hb_rate_lcl, hb_rate_ucl)
 
@@ -65,44 +67,44 @@ simd_rate_full <- read_excel(data_path, sheet = "data for tab 8",
 # Rates
 simd_rate <- simd_rate_full %>%
   select(1:7)
-names(simd_rate) <- c("Year", "All", "SIMD 1", "SIMD 2", "SIMD 3",
+names(simd_rate) <- c("date", "All", "SIMD 1", "SIMD 2", "SIMD 3",
                       "SIMD 4", "SIMD 5")
 simd_rate <- simd_rate %>%
-  filter(!is.na(Year)) %>%
+  filter(!is.na(date)) %>%
   pivot_longer(cols = All:`SIMD 5`,
-               names_to = "SIMD",
-               values_to = "Rate") %>%
-  mutate(Rate = round_half_up(as.numeric(Rate), 1))
+               names_to = "simd",
+               values_to = "indicator") %>%
+  mutate(indicator = round_half_up(as.numeric(indicator), 1))
 
 # Rates - LCL
 simd_rate_lcl <- simd_rate_full %>%
   select(1, 8:13)
-names(simd_rate_lcl) <- c("Year", "All", "SIMD 1", "SIMD 2", "SIMD 3",
+names(simd_rate_lcl) <- c("date", "All", "SIMD 1", "SIMD 2", "SIMD 3",
                           "SIMD 4", "SIMD 5")
 simd_rate_lcl <- simd_rate_lcl %>%
-  filter(!is.na(Year)) %>%
+  filter(!is.na(date)) %>%
   pivot_longer(cols = All:`SIMD 5`,
-               names_to = "SIMD",
-               values_to = "Rate_LCL") %>%
-  mutate(Rate_LCL = round_half_up(as.numeric(Rate_LCL), 1))
+               names_to = "simd",
+               values_to = "lower_confidence_interval") %>%
+  mutate(lower_confidence_interval = round_half_up(as.numeric(lower_confidence_interval), 1))
 
 # Rates - UCL
 simd_rate_ucl <- simd_rate_full %>%
   select(1, 14:19)
-names(simd_rate_ucl) <- c("Year", "All", "SIMD 1", "SIMD 2", "SIMD 3",
+names(simd_rate_ucl) <- c("date", "All", "SIMD 1", "SIMD 2", "SIMD 3",
                           "SIMD 4", "SIMD 5")
 simd_rate_ucl <- simd_rate_ucl %>%
-  filter(!is.na(Year)) %>%
+  filter(!is.na(date)) %>%
   pivot_longer(cols = All:`SIMD 5`,
-               names_to = "SIMD",
-               values_to = "Rate_UCL") %>%
-  mutate(Rate_UCL = round_half_up(as.numeric(Rate_UCL), 1))
+               names_to = "simd",
+               values_to = "upper_confidence_interval") %>%
+  mutate(upper_confidence_interval = round_half_up(as.numeric(upper_confidence_interval), 1))
 
 # Join together
 simd_rate <- simd_rate %>%
   left_join(simd_rate_lcl) %>%
   left_join(simd_rate_ucl) %>%
-  filter(Year >= 2008)
+  filter(date >= 2008)
 
 rm(simd_rate_full, simd_rate_lcl, simd_rate_ucl)
 
