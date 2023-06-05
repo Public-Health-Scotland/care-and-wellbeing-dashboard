599604290f78099513878fa7028c46c43c9db262
callid01 <calli.dougall@phs.scot>
2023-06-05 16:27:02 +0100

    Action links for home page buttons

---
+++ shiny_app/indicators/home/server_home.R
@@ -0,0 +1,28 @@
+# Navigation buttons on home page
+
+observeEvent(input$jump_to_child_start, {
+  updateTabsetPanel(session, "intabset", selected = "child_start")})
+
+observeEvent(input$jump_to_control_lives, {
+  updateTabsetPanel(session, "intabset", selected = "control_lives")})
+
+observeEvent(input$jump_to_fair_work, {
+  updateTabsetPanel(session, "intabset", selected = "fair_work")})
+
+observeEvent(input$jump_to_healthy_standard, {
+  updateTabsetPanel(session, "intabset", selected = "healthy_standard")})
+
+observeEvent(input$jump_to_sustainable_communities, {
+  updateTabsetPanel(session, "intabset", selected = "sustainable_communities")})
+
+observeEvent(input$jump_to_strengthen_prevention, {
+  updateTabsetPanel(session, "intabset", selected = "strengthen_prevention")})
+
+observeEvent(input$jump_to_discrimination_racism, {
+  updateTabsetPanel(session, "intabset", selected = "discrimination_racism")})
+
+observeEvent(input$jump_to_environmental_sustainability, {
+  updateTabsetPanel(session, "intabset", selected = "environmental_sustainability")})
+
+
+
