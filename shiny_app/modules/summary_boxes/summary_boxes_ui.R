237312202b445d5fff10a14c43142fdfe4d807eb
calumm07 <calum.massie2@phs.scot>
2023-06-06 14:57:11 +0100

    Making summary box titles for to be developed h4

--- shiny_app/modules/summary_boxes/summary_boxes_ui.R
+++ shiny_app/modules/summary_boxes/summary_boxes_ui.R
@@ -1,13 +1,13 @@
 
 
-summaryBoxUI <- function(id, title = "", button_title = "", button_content = "To be developed") {
+summaryBoxUI <- function(id, title = "To be developed", button_title = "", button_content = "To be developed") {
 
   ns <- NS(id)
 
   tagList(
     box(id = ns(id),
         status = "primary",
-        title = div(class = "summary-box-title", glue("{title}")),
+        title = h4(class = "summary-box-title", glue("{title}")),
         summaryButtonUI(ns("summary_info"),
                         button_title,
                         button_content),
