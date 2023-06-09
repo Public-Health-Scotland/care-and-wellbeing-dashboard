41563ccdbdba6b0b47826b66d6cb91ad2f304326
jennie04 <jennifer.evans4@phs.scot>
2023-06-09 14:46:58 +0100

    Change title ion ui to void creating an empty h3 header

--- shiny_app/modules/summary_boxes/summary_boxes_ui.R
+++ shiny_app/modules/summary_boxes/summary_boxes_ui.R
@@ -7,12 +7,13 @@ summaryBoxUI <- function(id, title = "To be developed", button_title = "", butto
   tagList(
     box(id = ns(id),
         status = "primary",
-        title = h4(class = "summary-box-title", glue("{title}")),
+      # title = "hi", #h4(class = "summary-box-title", glue("{title}")),
         summaryButtonUI(ns("summary_info"),
                         button_title,
                         button_content),
         width = "100%",
-        uiOutput(ns("summary_values"))
+      tagList(h4(class = "summary-box-title", glue("{title}")),
+        uiOutput(ns("summary_values")))
     ),
     br()
   )
