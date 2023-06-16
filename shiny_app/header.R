a8c67ed4afdeb4b76257bef02c09f95b4b04b9c3
jennie04 <jennifer.evans4@phs.scot>
2023-06-16 10:51:59 +0100

    add google-analytics (include in gitignore + call in google-analytics html in the app)

--- shiny_app/header.R
+++ shiny_app/header.R
@@ -1,3 +1,5 @@
 tags$head(includeCSS("www/styles.css"),  # CSS stylesheet
-                   tags$link(rel = "shortcut icon", href = "favicon_phs.ico"),# Icon for browser tab
-                   includeScript("www/javascript.js"))
+          tags$link(rel = "shortcut icon", href = "favicon_phs.ico"),# Icon for browser tab
+          includeScript("www/javascript.js"),
+          includeHTML("www/google-analytics.html")
+)
