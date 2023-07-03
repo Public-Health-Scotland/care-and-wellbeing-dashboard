7fc34c24751cfe335af7ae5167be517796ec42b8
jennie04 <jennifer.evans4@phs.scot>
2023-07-03 15:15:36 +0100

    update drug admissions data to be up to date

--- data_transfer/data_transfer_sections/transfer_drug_admissions.R
+++ data_transfer/data_transfer_sections/transfer_drug_admissions.R
@@ -2,17 +2,12 @@
 # DRUG RELATED HOSPITAL ADMISSIONS----
 ##############################################.
 
-drug_stays = read_csv(paste0(path_in_pop, "/Drug Standardised hospitals stays age_sex_trend_data.csv")) %>%
+drug_stays = read_csv(paste0(path_in_pop, "/Drug Standardised hospital stays age_sex_trend_data.csv")) %>%
   clean_names() %>%
   mutate(geography_type = "Scotland",
          geography = "Scotland",
-         sex = recode(sex, `Both sexes` = "All sexes"))
-
-
-drug_stays_summary = drug_stays %>%
-  filter(age_group == "All age groups") %>%
-  summary_format_function(., .$financial_year, .$geography_type, .$geography,
-                          round_half_up(.$rate, 1), "drug_admissions")
+         sex = recode(sex, `Both sexes` = "All sexes")) %>%
+  rename("rate" = "value") %>%
+  select(-measure)
 
 replace_file_fn(drug_stays, paste0(path_out, "/drug_stays.rds"))
-replace_file_fn(drug_stays_summary, paste0(path_out, "/drug_stays_summary.rds"))
